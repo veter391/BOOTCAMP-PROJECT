81e9c9cad1a8898d4ecf7b297d402971430985ac
joseignaciotipaldi <josetipaldi1@gmail.com>
2023-12-02 19:40:07 +0100

    .

--- back/src/config/queries/usersQueries.js
+++ back/src/config/queries/usersQueries.js
@@ -1,6 +1,7 @@
 const usersQueries = {
     createUser: 'INSERT INTO user (first_name, last_name, email, password, last_update, usertype) VALUES (?, ?, ?, ?, ?, ?)',
     getUserById: 'SELECT * FROM users WHERE id = ?',
+    getAllUsers: 'SELECT * FROM users',
     updateUser: 'UPDATE users SET first_name = ?, last_name = ?, email = ?, password = ?, last_update = ?, usertype = ? WHERE id = ?',
     deleteUser: 'DELETE FROM users WHERE id = ?'
 }
