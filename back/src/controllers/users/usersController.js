bc1437eb363e7d2ae88439cec94375df953875d6
josetipaldiok <josetipaldiok@gmail.com>
2023-12-04 09:03:36 +0100

    created schemas and validated user

--- back/src/controllers/users/usersController.js
+++ back/src/controllers/users/usersController.js
@@ -1,23 +1,25 @@
-
 import usersQueries from '../../config/queries/usersQueries.js';
+import userSchemas from './ruta/a/tu/schemas';
+
+const { CreateUserSchema, GetAllUsersSchema, GetUserByIdSchema, UpdateUserSchema, DeleteUserSchema } = userSchemas;
 
 //CREATE USER
 const createUser = async (req, res) => {
     try {
-        const { first_name, last_name, email, password, last_update, usertype } = req.body;
+        const { first_name, last_name, email, password, last_update, usertype } = CreateUserSchema.parse(req.body);
         const newUser = await usersQueries.createUser(first_name, last_name, email, password, last_update, usertype);
         res.status(201).json(newUser);
     } catch (error) {
-        res.status(500).json({ error: error.message });
-    }     
-     
+        res.status(400).json({ error: error.errors });
+    }
 };
 
 //GET ALL USERS
 const getAllUsers = async (req, res) => {
     try {
         const allUsers = await usersQueries.getAllUsers();
-        res.status(200).json(allUsers);
+        const validateUsers = GetAllUsersSchema.parse(allUsers);
+        res.status(200).json(validateUsers);
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
@@ -26,7 +28,7 @@ const getAllUsers = async (req, res) => {
 //GET USER{id}
 const getUserById = async (req, res) => {
     try {
-        const { id } = req.body;
+        const { id } = GetUserByIdSchema.parse(req.body);
         const user = await usersQueries.getUserById(id);
         if (user) {
             res.status(200).json(user);
@@ -34,29 +36,29 @@ const getUserById = async (req, res) => {
             res.status(404).json({ message: 'User not found' });
         }
     } catch (error) {
-        res.status(500).json({ error: error.message });
+        res.status(400).json({ error: error.errors });
     }
 };
 
 //UPDATE USER{id}
 const updateUser = async (req, res) => {
     try {
-        const { id, first_name, last_name, email, password, last_update, usertype } = req.body;
+        const { id, first_name, last_name, email, password, last_update, usertype } = UpdateUserSchema.parse(req.body);
         const updatedUser = await usersQueries.updateUser(id, first_name, last_name, email, password, last_update, usertype);
         res.status(200).json(updatedUser);
     } catch (error) {
-        res.status(500).json({ error: error.message });
+        res.status(400).json({ error: error.errors });
     }
 };
 
 //DELETE USER{id}
 const deleteUser = async (req, res) => {
     try {
-        const { id } = req.body;
+        const { id } = DeleteUserSchema.parse(req.body);
         await usersQueries.deleteUser(id);
         res.status(200).json({ message: 'User deleted successfully' });
     } catch (error) {
-        res.status(500).json({ error: error.message });
+        res.status(400).json({ error: error.errors });
     }
 };
 
