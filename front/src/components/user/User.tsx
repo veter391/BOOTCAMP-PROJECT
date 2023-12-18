a64cc67bd33061fdb523c18666dd7aac80d249e2
joseignaciotipaldiok <josetipaldiok@gmail.com>
2023-12-18 11:08:21 +0100

    ...

--- front/src/components/user/User.tsx
+++ front/src/components/user/User.tsx
@@ -1,9 +1,10 @@
+import { FormEvent, useContext, useState, useRef } from 'react';
 import './user.scss';
 import validationScheme from '../../helpers/validationScheme';
 import { InputValidate, AreaValidate } from '../inputValidate/InputValidate';
 import { AppContext } from '../../context/AppProvider';
-import { FormEvent, useContext, useState } from 'react';
 import createEvent from '../../services/events/createEvent';
+import uploadImageUser from '../../services/uploads/uploadsImageUser';
 
 interface FormData {
   title: string;
@@ -15,11 +16,60 @@ interface FormData {
 }
 
 function User () {
+  const { user }: any = useContext(AppContext);
   const [newEvent, setNewEvent] = useState(false);
+  const [avatar, setAvatar] = useState<string>('https://picsum.photos/100/100');
+  const fileInputRef = useRef<HTMLInputElement>(null);
+
+  const handleAvatarClick = (): void => {
+    if (fileInputRef.current) {
+      fileInputRef.current.click();
+    }
+  };
+
+  const handleAvatarChange = async (event: React.ChangeEvent<HTMLInputElement>): Promise<void> => {
+    const file = event.target.files?.[0];
+    if (file) {
+      try {
+        const newAvatar = URL.createObjectURL(file);
+        setAvatar(newAvatar);
+  
+        console.log('Nueva imagen seleccionada:', newAvatar);
+  
+        const userId = user?.id;
+        const userToken = user?.token;
+  
+        console.log(userId);
+        console.log(userToken);
+        
+        if (userId !== undefined && userId !== null && userToken) {
+          const response = await uploadImageUser(userId, file, userToken);
+          console.log('Imagen subida exitosamente:', response);
+        } else {
+          console.error('ID de usuario o token no válidos');
+        }
+      } catch (error: any) {
+        console.error('Error al subir la imagen:', error.message);
+      }
+    }
+  };
 
   return (
     <div className='user'>
-      <img src="https://picsum.photos/100/100" alt="avatar" />
+      <input
+        type='file'
+        ref={fileInputRef}
+        onChange={handleAvatarChange}
+        accept='image/*' //aceptar solo imagenes
+        id='avatarInput'
+        hidden
+      />
+      <img 
+        src="https://picsum.photos/100/100" 
+        alt="avatar" 
+        onClick={handleAvatarClick}
+        className='avatar-image'
+      />
       <h2>Username</h2>
       <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nam assumenda repudiandae, consequuntur velit laborum debitis saepe consequatur natus eligendi error.</p>
       {newEvent && <UserEvent />}
